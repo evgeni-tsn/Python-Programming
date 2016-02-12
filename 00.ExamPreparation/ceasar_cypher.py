b1b7453d2c191f741c5aceeb99865b8689f5f2e9
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2016-02-12 23:34:09 +0200

    Simplify one condition

--- 00.ExamPreparation/ceasar_cypher.py
+++ 00.ExamPreparation/ceasar_cypher.py
@@ -1,17 +1,14 @@
-shift = input()
-text = input()
 try:
+    shift = input()
+    text = input()
     def encrypt(message, shift):
         new = ''
         for i in (ord(x) for x in message):  # gets the ascii value of each letter in message
-            if 64 < i < 91:  # uppercase
+            if 64 < i < 91:  # uppercase letters
                 new += chr(65 + (i + shift - 65) % 26)
-            elif 96 < i < 123:  # lowercase
-                new += chr(97 + (i + shift - 97) % 26)
             else:
                 new += chr(i)
         return new
-
 
     print(encrypt(text, int(shift)))
 except:
